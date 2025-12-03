c7fa5bcb58f5fb53d7fff9621ce4076aab81b02f
kendrickgolden <kendrickgolden24@gmail.com>
2025-12-02 20:57:15 -0600

    implement multiset Jaccard

--- scripts/analyze-function-similarity.py
+++ scripts/analyze-function-similarity.py
@@ -4,17 +4,33 @@ print(sys.executable)
 
 from neo4j import GraphDatabase
 from itertools import combinations
+from collections import Counter
 
 URI = "neo4j://localhost:7687"
 AUTH = ("neo4j", "00000000")
 
 #Jaccard index measured by |A int. B| / |A union B|
-def find_jaccard_similarity(set1, set2):
+'''def find_jaccard_similarity(set1, set2):
     set_intersection = set1.intersection(set2)
     set_union = set1.union(set2)
     if not set_union:
         return 0
-    return len(set_intersection) / len(set_union)
+    return len(set_intersection) / len(set_union)'''
+
+#Jaccard index, while also accounting for duplicate AST node types
+def multiset_jaccard(list1, list2):
+    counter1 = Counter(list1)
+    counter2 = Counter(list2)
+
+    #calculcate the number of times a label occurs in both methods, and return sum over all labels
+    intersection_count = sum(min(counter1[k], counter2[k]) for k in counter1.keys() | counter2.keys())
+    #calculcate the number of times a label occurs in at least one method, and return sum over all labels
+    union_count = sum(max(counter1[k], counter2[k]) for k in counter1.keys() | counter2.keys())
+    
+    if union_count == 0:
+        return 0
+    return intersection_count / union_count
+
 
 with GraphDatabase.driver(URI, auth=AUTH) as driver:
     driver.verify_connectivity()
@@ -37,14 +53,14 @@ with GraphDatabase.driver(URI, auth=AUTH) as driver:
         for sublist in record["astTypes"]:
             for label in sublist:
                 labels.append(label)
-        functions[record["method"]] = set(labels)
+        functions[record["method"]] = labels
     print(functions.get("countA"))
 
 
     #look at every non-identical pair of functions and return those with Jaccard similiarty score greater than 0.5
     for f1, f2 in combinations(functions.keys(), 2):
-            sim = find_jaccard_similarity(functions[f1], functions[f2])
-            if sim > 0.5:  # threshold for "high similarity"
+            sim = multiset_jaccard(functions[f1], functions[f2])
+            if sim >= 0.0:  # threshold for "high similarity"
                 print(f"{f1} ~ {f2}: {sim:.2f}")
 
     # Summary information
